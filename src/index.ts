d7313b50cb2a6c8994fb93a0c22e4915a2b96396
Daniel Fischer <daniel@d-fischer.dev>
2020-01-04 16:58:51 +0100

    initial code

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Connection from './Connection';
+import DirectConnection from './DirectConnection';
+import WebSocketConnection from './WebSocketConnection';
+
+export { Connection, DirectConnection, WebSocketConnection };
